4db42a084dcbbeef63a9cefa1a0d222e3e1367c5
Narcissus2 <DMT1709533@xmu.edu.my>
2018-10-26 12:07:43 +0800

    Update ChessBoard.h

---
+++ ChessBoard.h
@@ -0,0 +1,16 @@
+#ifndef CHESSBOARD_H
+#define CHESSBOARD_H
+
+
+class ChessBoard
+{
+    public:
+        ChessBoard();
+        virtual ~ChessBoard();
+
+    protected:
+
+    private:
+};
+
+#endif // CHESSBOARD_H
